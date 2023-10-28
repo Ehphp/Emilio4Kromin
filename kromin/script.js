bbfc9bf6092b2ee64ba13351c615ee9ee341c001
emilio <hopalong.ph96@gmail.com>
2023-10-28 10:42:34 +0200

    commit to do list venerdi mattuna

--- kromin/script.js
+++ kromin/script.js
@@ -1,15 +1,33 @@
-const input = document.querySelector('#input');
+let input = document.querySelector('#input');
+
 const ul = document.querySelector('ul');
 const list = document.createElement('li')
 
 function aggiungAttivita() {
+    const date = new Date();
+    const dataInsertimento = date.toDateString();
     const list = document.createElement('li')
     list.textContent = input.value;
-    list.style.backgroundColor = getRandomColor();
-
+    const coloreRandom = getRandomColor();
+    list.style.backgroundColor = coloreRandom;
+    console.log(input.value)
     list.addEventListener('click', () => {
+        console.log(list)
         ul.removeChild(list)
+        const output = document.getElementsByClassName('output')
+        const tracUl = document.createElement('ul')
+        const tracLi = document.createElement('li')
+        // const date = new Date();
+        const dataCompletamento = date.toDateString();
+        tracLi.textContent = `la task ${list.innerText} inserita il ${dataInsertimento} è stata completata il ${dataCompletamento}`;
+        tracLi.classList.add('taskCompleted')
+        tracLi.style.color = coloreRandom;
+        tracLi.style.borderColor = coloreRandom;
+        tracUl.appendChild(tracLi)
+        output[0].appendChild(tracUl)
     })
+
+
     ul.appendChild(list)
     clenForm()
     event.preventDefault();
